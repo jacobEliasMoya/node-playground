5cbfadc0336873722e98d4ab04f4ac514cc99645
jacob moya <jemoya1994@Gmail.com>
2025-04-01 00:37:13 -0700

    chatgpt is an amazing teacher .. when used as if its a  Senior dev is wonderful

--- server.js
+++ server.js
@@ -11,9 +11,15 @@ app.get('/about',(req,res) => {
     res.sendFile(path.join(__dirname,'public','about.html'));
 });
 
+app.get('/search',(req,res) => {
+    res.send(`searched for: ${req.query.term} `);
+});
+
 app.listen(8001,()=> console.log('servers working on port 8001'))
 
 
 
 // so key points for me here
 // path utlity for filepaths, also using __dirname = current working directory .. using path.join(stuff), create the path without having backslashes and whatnot, seems useful
+
+// ahhh so app.get w/ a form using get, will run the req.send 
