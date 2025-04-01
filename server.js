944d86170fae75ff7f58b049abde8d9b5e116541
jacob moya <jemoya1994@Gmail.com>
2025-04-01 00:19:44 -0700

    I think this was a solid learning sesh, will continue tomorrow after work - starting to dive into the differences between static hosting vs server side routing

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.listen(8000,()=> console.log('servers working on port 8000'))
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'public','index.html'));
+});
+
+app.get('/about',(req,res) => {
+    res.sendFile(path.join(__dirname,'public','about.html'));
+});
+
+app.listen(8001,()=> console.log('servers working on port 8001'))
+
+
+
+// so key points for me here
+// path utlity for filepaths, also using __dirname = current working directory .. using path.join(stuff), create the path without having backslashes and whatnot, seems useful
