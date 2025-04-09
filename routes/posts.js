f0241b7053428fb653de62a23ed792626fb59add
jacob moya <jemoya1994@Gmail.com>
2025-04-08 22:59:12 -0700

    small bit of getting it in, breaking down with AI and undestanding the whys behind the code - backend seems rigid - but its the brain behind the magic that happens on the frontend so Im all in

--- routes/posts.js
+++ routes/posts.js
@@ -1,35 +1,57 @@
 import express from 'express';
 const router = express.Router();
 
-let posts = [ 
-    {id:1, title:'post-1'},
-    {id:2, title:'post-2'},
-    {id:3, title:'post-3'},
+let posts = [
+    { id: 1, title: 'post-1' },
+    { id: 2, title: 'post-2' },
+    { id: 3, title: 'post-3' },
 ]
 
-// get all posts
-router.get('/',(req,res)=>{
+const checkTitle = (string) => {
+    console.log(`hmmm this title: ${string} is interesting `)
+}
 
-    const limit =  parseInt(req.query.limit);   
+// get all posts
+router.get('/', (req, res) => {
+
+    const limit = parseInt(req.query.limit);
 
     // not needed to chain in that status, but is a good practice, it can  
-    if(!isNaN(limit) &&  limit > 0 ){
-        res.status(200).json(posts.slice(0,limit))
+    if (!isNaN(limit) && limit > 0) {
+        res.status(200).json(posts.slice(0, limit))
     } else {
         res.status(200).json(posts)
     }
 })
 
 // get single post
-router.get('/:id',(req,res)=>{
+router.get('/:id', (req, res) => {
 
     const id = parseInt(req.params.id);
     const post = posts.find((post) => post.id === id);
 
-    if(!post){
-        return res.status(404).json({msg:`a post with the id:${id} was not found`});
-    } 
+    if (!post) {
+        return res.status(404).json({ msg: `a post with the id:${id} was not found` });
+    }
     res.status(200).json(post)
 
 })
+
+// create new post
+router.post('/', (req, res) => {
+
+    const newPost = {
+        id: posts.id + 1,
+        title: req.body.title
+    };
+
+    if (!newPost.title) {
+        return res.status(400).json({ message: 'please include a title' })
+    }
+
+    checkTitle(newPost.title)
+
+    posts.push(newPost)
+});
+
 export default router;
--- server.js
+++ server.js
@@ -5,20 +5,9 @@ const port = process.env.PORT || 8000
 
 const app = express();
 
-// setup static folder :thinking face: lol
-// app.use(express.static(path.join(__dirname,'public')))
-
-// app.get('/',(req,res) => {
-//     res.sendFile(path.join(__dirname,'public','index.html'));
-// });
-
-// app.get('/about',(req,res) => {
-//     res.sendFile(path.join(__dirname,'public','about.html'));
-// });
-
-// app.get('/search',(req,res) => {
-//     res.send(`searched for: ${req.query.term} `);
-// });
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 
 app.use('/api/posts',posts)
 
